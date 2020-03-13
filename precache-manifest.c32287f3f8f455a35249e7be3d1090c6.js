f83e72fdbb0387dcb0b659ce6b7eabaafbbcdda0
Greg <gregory.tan@tradegecko.com>
2020-03-13 15:59:15 +0800

    Updates

---
+++ precache-manifest.c32287f3f8f455a35249e7be3d1090c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c03fb3a5230f6dfe4fd8a4d4dfbac2",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "945067d86ee5c02dc354",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d32eeeb079a4308012d4",
+    "url": "/demo/static/js/2.7904910b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/demo/static/js/2.7904910b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945067d86ee5c02dc354",
+    "url": "/demo/static/js/main.88f6479b.chunk.js"
+  },
+  {
+    "revision": "9b950d6ebfc8ae9fe495",
+    "url": "/demo/static/js/runtime-main.bcd6ecf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
