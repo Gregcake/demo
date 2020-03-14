fa3dbf46aa7a41cf49d1f5971830a6fa8ba5c19f
Greg <gregory.tan@tradegecko.com>
2020-03-14 15:30:24 +0800

    test with all

--- demo/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders hello world link', () => {
-  const { getByText } = render(<App />);
-  const element = getByText(/Hello, world!/i);
-  expect(element).toBeInTheDocument();
-});
